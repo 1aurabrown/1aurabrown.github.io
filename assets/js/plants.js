a6be93e5f82b1b27b6ea7e5f953244b8dfb88a6f
Laura Brown <laurabrown1113@gmail.com>
2017-10-20 16:08:48 +0200

    randomize

--- assets/js/plants.js
+++ assets/js/plants.js
@@ -1,4 +1,23 @@
+function shuffle(array) {
+  var m = array.length, t, i;
+
+  // While there remain elements to shuffle…
+  while (m) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
+}
+
 $(function() {
+  $(shuffle($('.plant-background')).first()).addClass('true');
   $('.plant-background').click(function(e){
     $el = $(e.currentTarget);
     $el.removeClass('true');
