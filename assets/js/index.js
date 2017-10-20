7cd490ca926f83099e4edfb0231932d3ce146022
Laura Brown <laurabrown1113@gmail.com>
2017-10-20 14:41:43 +0200

    fix grid moving on expansion

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,11 @@
 $(function(){
-  $('.images-grid .image').click(function(e){
+  $('.images-grid .image.thumb').click(function(e){
     $el = $(e.currentTarget)
     if($el.hasClass('expanded')){
       $el.removeClass('expanded');
     } else {
       $('.images-grid .image.expanded').removeClass('expanded');
-      $(e.currentTarget).addClass('expanded');
+      $el.addClass('expanded');
     }
   })
 })
